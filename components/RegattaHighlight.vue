83057d8d11f191de4506d52fae989d2c489b045f
Gerben Mulder <gerbenmulder2508@gmail.com>
2022-01-06 16:13:46 +0100

    change to composite

---
+++ components/RegattaHighlight.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="card">
+    <span id="title">{{ text }}</span>
+    <div id="contents">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  text: { type: String, default: "Default Headline" },
+});
+</script>
+
+<style scoped>
+#title {
+  font-size: larger;
+  color: black;
+}
+
+#card {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  width: 100%;
+  height: 100%;
+  border-radius: var(--corner-radius-large);
+  background: var(--secondary-color);
+  padding: 1rem 1rem;
+}
+
+#contents {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  justify-content: center;
+}
+</style>
