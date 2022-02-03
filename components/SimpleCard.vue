37bbac2259a7b1e279303b01f6fa8d56ebb96a27
Gerben Mulder <gerbenmulder2508@gmail.com>
2022-02-03 19:49:04 +0100

    Fix image path

--- components/SimpleCard.vue
+++ components/SimpleCard.vue
@@ -13,7 +13,7 @@
 <script setup lang="ts">
 defineProps({
   title: { type: String, default:"Default title" },
-  logo:  { type: String, default:"assets/logo.png" },
+  logo:  { type: String, default:"public/logo.png" },
 });
 </script>
 
