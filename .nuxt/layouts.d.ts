35092645e4e5b2a585e091d02392587ba218cf87
Gerben Mulder <gerbenmulder2508@gmail.com>
2022-05-17 21:55:03 +0200

    Delete .nuxt directory

--- .nuxt/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComputedRef, Ref } from 'vue'
-export type LayoutKey = "default" | "noheader"
-declare module '/Users/gerbuuun/Development/Proteus-Eretes/hswi2/node_modules/nuxt3/dist/pages/runtime/composables' {
-  interface PageMeta {
-    layout?: false | LayoutKey | Ref<LayoutKey> | ComputedRef<LayoutKey>
-  }
-}
