595ea298d879bc54974e5e4e2c0237794e3d8df7
cornelis <cornelis.zwart.cz@gmail.com>
2022-08-01 23:00:18 +0200

    draw api models made

---
+++ models/draw.ts
@@ -0,0 +1,24 @@
+import { Time } from '~/models/time';
+
+export interface DrawItem {
+    knrbid: string;
+    teamname : string;
+    fieldnameshort: string;
+    fieldnameshortsub: string;
+    clubname : string;
+    clubnameshort: string;
+    slotid : string;
+    raceid : string;
+    tossorder : number;
+    backnumber : number;
+    comments : string | null;
+    rower8: string | null;
+    coachnames : string | null;
+}
+
+export interface DrawGet {
+    error: boolean;
+    message: string;
+    setting: {};
+    teams: DrawItem[];
+}
