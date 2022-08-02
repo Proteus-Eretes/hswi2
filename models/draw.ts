776d3ae8da2046c0311df762ae817165fae49ea8
cornelis <cornelis.zwart.cz@gmail.com>
2022-08-02 14:54:01 +0200

    prettier

--- models/draw.ts
+++ models/draw.ts
@@ -1,24 +1,24 @@
 import { Time } from '~/models/time';
 
 export interface DrawItem {
-    knrbid: string;
-    teamname : string;
-    fieldnameshort: string;
-    fieldnameshortsub: string;
-    clubname : string;
-    clubnameshort: string;
-    slotid : string;
-    raceid : string;
-    tossorder : number;
-    backnumber : number;
-    comments : string | null;
-    rower8: string | null;
-    coachnames : string | null;
+  knrbid: string;
+  teamname: string;
+  fieldnameshort: string;
+  fieldnameshortsub: string;
+  clubname: string;
+  clubnameshort: string;
+  slotid: string;
+  raceid: string;
+  tossorder: number;
+  backnumber: number;
+  comments: string | null;
+  rower8: string | null;
+  coachnames: string | null;
 }
 
 export interface DrawGet {
-    error: boolean;
-    message: string;
-    setting: {};
-    teams: DrawItem[];
+  error: boolean;
+  message: string;
+  setting: {};
+  teams: DrawItem[];
 }
